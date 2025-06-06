17aaead3b5be788f93266bb8b461233582f67531
Victor Bugarski <bugarskivictor@gmail.com>
2025-06-06 16:13:13 +0200

    fixat med en bild

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import Logo from './Symbol-Ventixe.svg'
+import Logo from './Symbol-ventixe.svg'
 
 const Nav = () => {
   return (
     <nav>
         <div className="logo-section">
-        <img src={Logo} alt="Symbol Ventixe" className="logo" />
+        <img src={Logo} alt="Symbol-ventixe" className="logo" />
         <h2 className="brand">Ventixe</h2>
         </div>
         <NavLink to="/">Events</NavLink>
